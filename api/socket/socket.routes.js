3217d050f4586e4b5019e68db959fe3a25377b73
reemnx <37637982+reemnx@users.noreply.github.com>
2020-05-25 20:40:44 +0300

    reem

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -5,23 +5,52 @@ let gChats = [];
 
 function connectSockets(io) {
     io.on('connection', socket => {
-        console.log('connected');
+        console.log('User connected');
 
+        //Events
+        socket.on('nav mounted', (msg) => {
+            console.log('message: ' + msg);
+            io.emit('nav mounted', msg);
+          });
 
-        socket.on('chat newMsg', msg => {
-            console.log('rommmmmm', socket.myTopic)
-            // io.emit('chat addMsg', msg)
-            // emits only to sockets in the same room
-            io.to(socket.myTopic).emit('chat addMsg', msg)
-        })
-        socket.on('chat setRoom', toyId => {
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
-            }
-            socket.join(toyId)
-            socket.myTopic = toyId;
-            console.log('sockett',socket.myTopic);
-            
-        })
+        socket.on('board updated', (id) => {
+            socket.broadcast.emit('board updated' , id);
+          });
+
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
     })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// socket.on('chat newMsg', msg => {
+//     console.log('rommmmmm', socket.myTopic)
+//     // io.emit('chat addMsg', msg)
+//     // emits only to sockets in the same room
+//     io.to(socket.myTopic).emit('chat addMsg', msg)
+// })
+// socket.on('chat setRoom', toyId => {
+//     if (socket.myTopic) {
+//         socket.leave(socket.myTopic)
+//     }
+//     socket.join(toyId)
+//     socket.myTopic = toyId;
+//     console.log('sockett',socket.myTopic);
+
+// })
